1be4b426c1a2f6a9b05660cf564016614b318677
Jaffer Sheriff <jaffer-2687@jaffer-2687.csez.zohocorpin.com>
2016-12-07 12:16:50 +0530

    Initial Commit

---
+++ TableViewInsideCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewInsideCell
+//
+//  Created by Jaffer Sheriff U on 07/12/16.
+//  Copyright © 2016 Jaffer Sheriff U. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewInsideCell/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TableViewInsideCell
+//
+//  Created by Jaffer Sheriff U on 07/12/16.
+//  Copyright © 2016 Jaffer Sheriff U. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
